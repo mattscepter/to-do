ba7eb24bc807567df341e10699813a820ec01137
mattscepter <chaudharyut25@gmail.com>
2022-01-02 12:50:20 +0530

    first commit

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 /**
  * Reading Environment Variables
  */
-import { config } from "dotenv";
+import { config } from 'dotenv';
 config();
 
 export default {
   database: {
     connectionLimit: 10,
-    host: process.env.DATABASE_HOST || "localhost",
-    user: process.env.DATABASE_USER || "root",
-    password: process.env.DATABASE_PASSWORD || "faztpassword",
-    database: process.env.DATABASE_NAME || "dblinks",
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: process.env.DATABASE_NAME || 'dblinks',
   },
   port: process.env.PORT || 4000,
 };
